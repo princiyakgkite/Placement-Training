7c139e4e790f010cf746b67dd9e9ac68903f72ef
Abiprinci <abiprinciya@gmail.com>
2026-09-23 16:14:13 +0530

    Added 2D array programs

---
+++ Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50, 60};
+
+        for (int i = 0; i < a.length; i += 2) {
+            System.out.println(a[i]);
+        }
+    }
+}
